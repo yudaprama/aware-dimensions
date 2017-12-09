14cd4d0d48e1a93b5297e48f41d0fea82dfbf6ae
yuda <yudhaprama@icloud.com>
2017-12-10 00:12:39 +0700

    aware-dimensions

---
+++ AwareDimensions.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+module.exports = function AwareDimensions(ComposedComponent) {
+  return class extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        width: 0,
+        height: 0,
+        initialRender: false
+      };
+      this.measure = this.measure.bind(this);
+    }
+
+    measure({nativeEvent : {layout : {width = 0, height = 0} = {}} = {}}){
+      this.setState({width, height, initialRender:true});
+    }
+
+    render(){
+      return (
+        this.state.initialRender ?
+          <ComposedComponent {...this.props} {...this.state} onLayout={this.measure}/> :
+          <View style={this.props.style} onLayout={this.measure} />
+      );
+    }
+  }
+}
